600c160fe8f0e179b2a17872adf66271cbd0494a
Samuel Lao <samuel.lao100@gmail.com>
2024-06-28 21:19:20 -0700

    attempting to make firebase work

--- App.jsx
+++ App.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomePage from "./pages/HomePage"
-import DebugPage from "./pages/DebugPage"
-import StudyPage from "./pages/StudyPage"
-import CameraPage from "./pages/CameraPage"
+import HomePage from "./pages/Home"
+import DebugPage from "./pages/Debug"
+import StudyPage from "./pages/Study"
+import CameraPage from "./pages/Camera"
+import SignUp from './pages/SignUp';
 
+import auth from '@react-native-firebase/auth';
 
 
 const Stack = createStackNavigator();
 
-
-
 export default function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    console.log(user)
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+  
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false, gestureEnabled: false, }}>
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Debug" component={DebugPage} />
-        <Stack.Screen name="Camera" component={CameraPage} />
-        <Stack.Screen name="Study" component={StudyPage} />
+        {user 
+          ? <>
+            <Stack.Screen name="Home" component={HomePage} />
+            <Stack.Screen name="Debug" component={DebugPage} />
+            <Stack.Screen name="Camera" component={CameraPage} />
+            <Stack.Screen name="Study" component={StudyPage} />
+          </>
+          : <>
+            <Stack.Screen name="SignUp" component={SignUp} />
+          </>
+        }
+
       </Stack.Navigator>
     </NavigationContainer>
   );
--- pages/Debug.jsx
+++ pages/Debug.jsx
@@ -1,5 +1,6 @@
 import { View, Text, SafeAreaView, Pressable } from 'react-native'
-import React from 'react'
+
+import React, {useState, useEffect} from 'react'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import Config from "react-native-config"
 
@@ -9,6 +10,11 @@ export default function DebugPage({navigation}) {
 
   const genAI = new GoogleGenerativeAI(Config.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { responseMimeType: "application/json" }});
+
+  const [response, setResponse] = useState(null)
+  useEffect(() => {
+    
+  }, [])
   
 
   const define = async (word, language) => {
@@ -27,7 +33,8 @@ export default function DebugPage({navigation}) {
     const result = await model.generateContent(prompt);
     const response = result.response;
     const text = response.text();
-    console.log(JSON.parse(text));
+    setResponse(text)
+    // console.log(JSON.parse(text));
   }
 
   return (
@@ -39,6 +46,7 @@ export default function DebugPage({navigation}) {
       <Pressable onPress={() => {navigation.navigate("Home")}}>
         <Text>Go to home</Text>
       </Pressable>
+      <Text>{response}</Text>
     </SafeAreaView>
   )
 }
